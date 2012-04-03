81804b45761dac27a7e7b83d81cea11d396797fb
Tom Hermann <tomhermann@gmail.com>
2012-04-02 19:11:16 -0500

    Cleanup before push

--- src/main/java/com/zombietank/email/velocity/VelocityEmailService.java
+++ src/main/java/com/zombietank/email/velocity/VelocityEmailService.java
@@ -1,6 +1,5 @@
 package com.zombietank.email.velocity;
 
-import static com.zombietank.email.EmailPreparator.preparable;
 import static org.springframework.ui.velocity.VelocityEngineUtils.mergeTemplateIntoString;
 
 import java.io.Serializable;
@@ -10,43 +9,31 @@ import org.apache.velocity.app.VelocityEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.scheduling.annotation.Async;
 
 import com.zombietank.email.Email;
+import com.zombietank.email.exception.EmailException;
 import com.zombietank.email.support.AbstractTemplatedEmailService;
 
 public class VelocityEmailService extends AbstractTemplatedEmailService {
 	private static final Logger log = LoggerFactory.getLogger(VelocityEmailService.class);
-	private final JavaMailSender mailSender;
 	private final VelocityEngine velocityEngine;
 
 	public VelocityEmailService(JavaMailSender mailSender, VelocityEngine velocityEngine) {
-		this.mailSender = mailSender;
+		super(mailSender);
 		this.velocityEngine = velocityEngine;
 	}
 	
-	@Async
-	public void send(Email email) {
-		log.info("Sending email to: {}", email.getTo());
-		log.debug("Sending: {}", email);
-	
-		try {
-			mailSender.send(preparable(email));
-			log.info("Sent email to: {}", email.getTo());
-		} catch(Exception e) {
-			log.error("Failed: email not sent to: {}", email.getTo(), e);
-		}
-	}
-	
-	public void send(Email email, String templateLocation, Map<String, ? extends Serializable> model) {
+	@Override
+	public void send(Email email, String templateLocation, Map<String, ? extends Serializable> model) throws EmailException {
 		log.debug("Using templateLocation: {}", templateLocation);
 		log.debug("Using model: {}", model);
 
 		try {
 			String message = mergeTemplateIntoString(velocityEngine, templateLocation, model);
+			log.debug("Merged message: {}", message);
 			send(email.withMessage(message));
 		} catch(Exception e) {
-			log.error("Failed: email not sent to: {}", email.getTo(), e);
+			throw new EmailException("Failed: email not sent to: " + email.getTo(), e);
 		}
 	}
 }
